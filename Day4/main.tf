ab63f9d841b0703609476499fc8149cffc1b22bb
Manoj G M <manojgm1812002@gmail.com>
2026-06-26 01:32:57 +0530

    Day-4 updated

---
+++ Day4/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "terraform-state-bucket"
+    key          = "dev/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  access_key = "test"
+  secret_key = "test"
+
+  s3_use_path_style           = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    s3 = "http://localhost:4566"
+  }
+}
+
+resource "aws_s3_bucket" "test_backend" {
+  bucket = "my-demo-bucket"
+
+  tags = {
+    Name        = "My bucket 2.0"
+    Environment = "Dev"
+  }
+}
